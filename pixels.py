44f9a6137dedbe67f7f4472e2a19e88d8f4f0515
Sam O'Neal <sam.oneal@gmail.com>
2022-12-04 19:24:29 -0800

    Color generator changed to decouple gradient and color blocks.

--- pixels.py
+++ pixels.py
@@ -1,14 +1,21 @@
+import numpy as np
 from PIL import ImageColor
 
-COLORS = ['red','orange','yellow','lime','cyan','blue','purple','magenta']
+COLORS = ['red','orange','purple','magenta','yellow','lime','cyan','blue']
 
-'''
-Returns a generator the builds the flat squence of pixel values for a grayscale image.
-Yields only a single value; this is not suitable for populating multiple channels.
-'''
 def bw_pixel_generator(tex_res):
-    step = tex_res // 8
+    """Returns a generator the builds the flat squence of pixel values for a grayscale image.
+Yields only a single value; this is not suitable for populating multiple channels.
+
+    Args:
+        tex_res (int): resolution
+
+    Yields:
+        int: pixel value
+    """
+    step = min(tex_res // 8, 16)
     sub_area = step**2
+
     for i in range(tex_res ** 2):
         #position in image
         global_x = i % tex_res
@@ -19,53 +26,80 @@ def bw_pixel_generator(tex_res):
         #gradient value
         total_steps = (step * local_y) + local_x
         score = total_steps/sub_area
-        score = (1 - score)
         #as int8
-        value = round(255 * score)
+        value = round(255 * (1 - score))
         yield value
 
-'''
-Returns RGB pixels one channel at a time. Total values will be 3x the amount of pixels.
-'''
 def color_pixel_generator(tex_res):
-    step = tex_res//8
-    sub_area = step**2
+    """Returns RGB pixels one channel at a time. Total values will be 3x the amount of pixels.
+
+    Args:
+        tex_res (int): resolution
+
+    Yields:
+        int: channel value
+    """
+    gradient_step = min(tex_res//8, 16)
+    sub_area = gradient_step**2
+
+    if tex_res > 64:
+        color_res = 8
+    elif tex_res >16:
+        color_res = 4
+    else:
+        color_res = 2
+
+    color_step = tex_res // color_res
+
     for i in range(tex_res ** 2):
         #position in image
         global_x = i % tex_res
         global_y = i // tex_res
-        #color block
-        block_index = i // step
-        block_index %= step
-        block_index += global_y // step
-        #keep index in range
-        block_index %= step
         #position in local gradient
-        local_x = global_x % step
-        local_y = global_y % step
+        local_x = global_x % gradient_step
+        local_y = global_y % gradient_step
         #gradient value
-        total_steps = (step * local_y) + local_x
+        total_steps = (gradient_step * local_y) + local_x
         score = total_steps/sub_area
         score = (1 - score)
+        #color block
+        color_x = global_x // color_step
+        color_y = global_y // color_step
+        color_index = (color_res * color_y) + color_x
+        #keep index in range
+        color_index %= 8
         #pick color from list
-        color = scale_color(ImageColor.getrgb(COLORS[block_index]), score)
+        color = scale_color(ImageColor.getrgb(COLORS[color_index]), score)
         for channel in color:
             yield channel
 
-
 def scale_color(pure_color, scale):
+    """Lighten or darken a color.
+
+    Args:
+        pure_color sequence: channels
+        scale (float): value between 0 and 1.0
+
+    Returns:
+        tuple: scaled channels
+    """
     color = [int(i) for i in pure_color]
+    #don't want pure white or pure black back when given a color
+    scale = np.interp(scale, [0, 1], [0.01, 0.99])
+
     if scale > 0.5:
         #lighten, increase if not 255
         amount = (scale - 0.5) * 2
-        for i in range(3):
-            if color[i] < 255:
+        for i, channel in enumerate(color):
+            if channel == 0:
                 color[i] = round(255 * amount)
+            elif channel < 255:
+                color[i] = min(color[i] + round(color[i] * scale), 255)
     elif scale < 0.5:
         #darken, decrease if not 0
         amount = (scale * 2)
-        for i in range(3):
-            if color[i]:
+        for i, channel in enumerate(color):
+            if channel:
                 color[i] = round(color[i] * amount)
     else:
         return tuple(pure_color)
@@ -73,9 +107,18 @@ def scale_color(pure_color, scale):
 
 if __name__ == '__main__':
     my_color = (255,0,0)
-    print(scale_color(my_color, 0.25))
-    print(scale_color(my_color, 0.75))
-    print(scale_color(my_color, 0.5))
-    print(scale_color(my_color, 0.99))
     print(scale_color(my_color, 0.01))
-
+    print(scale_color(my_color, 0.25))
+    print(scale_color(my_color, 0.45))
+    print(scale_color(my_color, 0.5))
+    print(scale_color(my_color, 0.55))
+    print(scale_color(my_color, 0.75))
+    print(scale_color(my_color, 0.99))
+    my_color = (255,128,0)
+    print(scale_color(my_color, 0.01))
+    print(scale_color(my_color, 0.25))
+    print(scale_color(my_color, 0.45))
+    print(scale_color(my_color, 0.5))
+    print(scale_color(my_color, 0.55))
+    print(scale_color(my_color, 0.75))
+    print(scale_color(my_color, 0.99))
--- tests/test_pixels.py
+++ tests/test_pixels.py
@@ -1,1 +1,20 @@
 from pixels import scale_color
+from PIL import ImageColor
+
+def test_scale_color():
+    my_color = ImageColor.getrgb('blue')
+
+    assert scale_color(my_color, 0) != (0,0,0)
+    assert scale_color(my_color, 1) != (255,255,255)
+    assert scale_color(my_color, 0.5) == my_color
+
+    assert my_color > scale_color(my_color, 0.4)
+    assert my_color < scale_color(my_color, 0.6)
+
+    my_color = ImageColor.getrgb('orange')
+    assert scale_color(my_color, 0) != (0,0,0)
+    assert scale_color(my_color, 1) != (255,255,255)
+
+    assert scale_color(my_color, 0.5) == my_color
+    assert my_color > scale_color(my_color, 0.4)
+    assert my_color < scale_color(my_color, 0.6)
